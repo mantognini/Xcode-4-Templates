28b9e057759b808e31c2680bcdb8021d442e6c8e
Marco Antognini <antognini.marco@gmail.com>
2012-04-06 20:15:26 +0200

    refactors C++ Class template

--- Templates/File Templates/C and C++/C++ Class.xctemplate/___FILEBASENAME___.hpp
+++ Templates/File Templates/C and C++/C++ Class.xctemplate/___FILEBASENAME___.hpp
@@ -6,10 +6,10 @@
 //  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
 //
 
-#ifndef ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER____
-#define ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER____
+#ifndef ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER___
+#define ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER___
 
-class ___FILEBASENAME___ {
+class ___VARIABLE_className:identifier___ {
     <#instance variables#>
     
 public:
