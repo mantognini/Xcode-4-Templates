88cfe7ed94a3ea9162508e7d08ccbb6dbac93acd
Marco Antognini <antognini.marco@gmail.com>
2012-04-06 16:42:35 +0200

    adds "C++ Class" template

---
+++ Templates/File Templates/C and C++/C++ Class.xctemplate/___FILEBASENAME___.hpp
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#ifndef ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER____
+#define ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER____
+
+class ___FILEBASENAME___ {
+    <#instance variables#>
+    
+public:
+    <#member functions#>
+};
+
+#endif
