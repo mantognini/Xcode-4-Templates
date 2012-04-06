5e9233a5db9bf694d7a14f38746945f20482d6bf
Marco Antognini <antognini.marco@gmail.com>
2012-04-06 21:25:29 +0200

    adds basic support for subclassing

---
+++ Templates/File Templates/C and C++/C++ Class.xctemplate/Yes/___FILEBASENAME___.hpp
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#ifndef ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER___
+#define ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER___
+
+class ___VARIABLE_className:identifier___ : public ___VARIABLE_superclassName:identifier___ {
+    <#instance variables#>
+    
+public:
+    <#member functions#>
+};
+
+#endif
